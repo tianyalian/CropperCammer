a871d78701f0fd55e6bb84c6fafcacbfd978e803
wy <yuge5506@qq.com>
2017-05-23 17:58:33 +0800

    添加背景阴影图层

---
+++ app/src/main/java/business_android_client/croppercammer/view/shadow.java
@@ -0,0 +1,24 @@
+package business_android_client.croppercammer.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by seeker on 2017/5/23.
+ */
+
+public class shadow extends View {
+    public shadow(Context context) {
+        super(context);
+    }
+
+    public shadow(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public shadow(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
