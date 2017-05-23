fdda9d2c556930558e961a1221a1bdcd14acc8cd
wy <yuge5506@qq.com>
2017-05-23 17:37:57 +0800

    Initial commit

---
+++ app/src/main/java/business_android_client/croppercammer/MainActivity.java
@@ -0,0 +1,13 @@
+package business_android_client.croppercammer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
